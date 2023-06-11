6e34a54e664afc9af910e53b002a4c908c2d3469
sahan <dsahan548@gmail.com>
2023-06-11 21:43:17 +0530

    Added funcion was created succesful

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -22,5 +22,13 @@ namespace BulkyWeb.Controllers
 		{
 			return View();
 		}
+
+		//added category save to databas and redirect to the view page again...
+		[HttpPost]
+		public IActionResult Create(Category obi)
+		{  _db.Categories.Add(obi);
+			_db.SaveChanges();
+			return RedirectToAction("Index","Category");  //no need to mention controller name if both action are genereted same controller....
+		}
 	}
 }
